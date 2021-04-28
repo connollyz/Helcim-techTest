06157ad65ca113083662a46c22784e70830a398c
Zoe <zoe_connolly@hotmail.com>
2021-04-27 21:53:16 -0600

    updated js

--- script/script.js
+++ script/script.js
@@ -4,6 +4,9 @@ const clearInput = function () {
    //imgInput
   const previewDisplay = document.getElementById('previewDisplay');
       previewDisplay.src = "";
+      // imgInput
+       const newImg = document.getElementById('newImg');
+                newImg.value = "";
 }
 
    const display = function(event) {
@@ -23,7 +26,8 @@ const clearInput = function () {
                 const newImg = document.getElementById('newImg');
                 newImg.value = "";
       })
-  
+
+
 
 
 
@@ -32,9 +36,11 @@ const post = document.getElementById('post').addEventListener('click', function(
   // Prevent refresh on submit
   event.preventDefault();
   
- 
+ //userAccountInfo
+  const userName = "John Doe"
+  const userProfileImg="assets/userIcon.svg"
   //imgInput
-   const newImg = document.getElementById('newImg');
+  const newImg = document.getElementById('newImg');
   const imgSrc = document.getElementById("previewDisplay").src
   //textInput
   let userTextInput = document.getElementById('userInput').value;
@@ -48,28 +54,37 @@ const post = document.getElementById('post').addEventListener('click', function(
   
  //Create NewCard Elements
   const newCard  = document.createElement('div');//newCard div
+  const author  =   document.createElement('div');//newCard userInfo
   const cardImg  =   document.createElement('div');//newCard Img div
-   const newDate = document.createElement('p');// newCard Date
+  const newDate = document.createElement('p');// newCard Date
   const newTextPost = document.createElement('p');// newCard Text p
   
   //add Class names to New Elements
   newCard.className+='card';
+  author.className+='author';
   cardImg.className+='postImg';
-   newDate.className+='date';
+  newDate.className+='date';
  
   // add content in to the html
+  author.innerHTML = `<div class="authorProfileImg">
+                          <img src=${userProfileImg}>
+                      </div>
+                      <h2>${userName}</h2>
+                      `;
   cardImg.innerHTML = `<img src=${imgSrc}>`;
-  newDate.innerHTML = `${months[m]}-${d}-${y}`;
-  newTextPost.innerHTML = `${userTextInput}`;
+  newDate.innerHTML = `<p>${months[m]}-${d}-${y}</p>`;
+  newTextPost.innerHTML = `<p>${userTextInput}</p>`;
   
 
 
   
   
   //Append Child Element to newCard
-   newCard.appendChild(cardImg);//Img
-   newCard.appendChild(newDate); //Date
-   newCard.appendChild(newTextPost);//Text
+
+  newCard.appendChild(author);//author of post
+  newCard.appendChild(cardImg);//Img
+  newCard.appendChild(newDate); //Date
+  newCard.appendChild(newTextPost);//Text
 
   //Append New Card to Newsfeed
   document.getElementById('news').appendChild(newCard);
@@ -85,4 +100,6 @@ const post = document.getElementById('post').addEventListener('click', function(
   }
   //Clear Input
   clearInput();
-});
+
+    
+    });
